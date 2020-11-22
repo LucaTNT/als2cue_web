a113771e8d12d79c0fc1c30ab53d7f7ea7af7b47
Luca Zorzi <luca@lucazorzi.net>
2020-11-22 20:13:45 +0100

    Added support for variable tempo projects

--- app/als2cue.py
+++ app/als2cue.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-import gzip, base64, os
-from xml.dom import minidom
+import alsParser, base64, sys
 
 def leadingZero(number, digits = 2):
     return str(number).zfill(digits)
 
-def getOffsetTime(time, tempo, getFrames = True):
-    time = time / (tempo / 60)
-    minutes = int(time / 60)
-    seconds = int(round(time - minutes * 60, 0))
+def formatTimestamp(total_seconds, getFrames = True):
+    minutes = int(total_seconds / 60)
+    seconds = int(round(total_seconds - minutes * 60, 0))
     frames = 0
 
     if getFrames:
@@ -16,38 +14,29 @@ def getOffsetTime(time, tempo, getFrames = True):
     else:
         return "%s:%s" % (leadingZero(minutes), leadingZero(seconds))
 
-def getChapters(als, filename):
+def getChapters(raw_data, filename):
     try:
-        raw_data = gzip.decompress(als)
-        doc = minidom.parseString(raw_data)
-        tempo = float(doc.getElementsByTagName('Tempo')[0].getElementsByTagName('Manual')[0].getAttribute('Value'))
-
-        locators = doc.getElementsByTagName('Locator')
+        als = alsParser.raw2xml(raw_data)
+        tempo_changes = alsParser.getTempoChanges(als)
+        tempo_intervals = alsParser.getTempoIntervals(tempo_changes)
+        locators = alsParser.getLocators(als)
 
         if len(locators) < 1:
             return (False, "No Ableton markers found")
 
+        chapters = []
         cue = "FILE \"%s.mp3\" MP3\n" % filename
 
-        timestamps = [0.0]
-        chapters = []
-
-        for locator in locators:
-            time = float(locator.getElementsByTagName('Time')[0].getAttribute('Value'))
-            timestamps.append(time)
-
-        timestamps.sort()
-
         j = 0
-        for time in timestamps:
+        for time in locators:
             j += 1
             cue += "    TRACK %s AUDIO\n" % leadingZero(j)
             cue += "        TITLE \"\"\n"
-            cue += "        INDEX 01 %s\n" % getOffsetTime(time, tempo)
+            cue += "        INDEX 01 %s\n" % formatTimestamp(time, alsParser.getOffsetTime(time, tempo_intervals))
             if j > 1:
-                chapters.append({"chapter_number": j - 1, "chapter_start": getOffsetTime(time, tempo, False)})
+                chapters.append({"chapter_number": j - 1, "chapter_start": formatTimestamp(alsParser.getOffsetTime(time, tempo_intervals), False)})
 
         return (True, chapters, cue, base64.b64encode(cue.encode('utf-8')).decode('utf-8'))
 
-    except Exception as e:
+    except:
         return (False, "Can't read Ableton project")
